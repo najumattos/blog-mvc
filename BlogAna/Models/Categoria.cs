598eab19763883f6f866c92d64c697c0325857cf
AnaJuliaMattos <anajuliamattos02@gmail.com>
2025-08-08 20:34:38 -0300

    Postagem e Categoria

---
+++ BlogAna/Models/Categoria.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogAna.Models;
+public class Categoria
+{
+    public int Id { get; set; }    
+    
+    [Required]
+    [StringLength(30)]
+    public string Nome { get; set; }
+}
