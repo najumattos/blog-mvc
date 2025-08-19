a74f8df00384be07750a0e04f0723cd03f866576
Ana Julia Mattos <anajuliamattos02@gmail.com>
2025-08-18 22:33:18 -0300

    :sparkles: feat: Criando a página de postagem

--- BlogAna/Controllers/HomeController.cs
+++ BlogAna/Controllers/HomeController.cs
@@ -7,30 +7,24 @@ namespace BlogAna.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private List<Postagem> postagens;
+    private List<Categoria> categorias;
+    
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
+         categorias = [
+            new(){ Id = 1, Nome = "Tecnologia"},
+            new(){ Id = 2, Nome = "Literatura"},
+            ];
 
-    public IActionResult Index()
-    {
-        ViewData["Mensagem"] = "Olá Mundo";
 
-
-        Categoria tecnologia = new()
-        {
-            Id = 1,
-            Nome = "Tecnologia"
-        };
-
-        Categoria literatura = new(2, "Literatura");
-
-        List<Postagem> postagens = [
+         postagens = [
             new(){
                 Id = 1,
                 Nome = "Avanços em Chips de IA: ",                
-                Categoria = tecnologia,
+                Categoria = categorias.Find(c => c.Id == 1),
                 DataPostagem = DateTime.Parse("07/08/2025"),
                 Descricao = "Novidades",
                 Texto = "Grandes empresas de tecnologia estão investindo pesado em chips especializados para inteligência artificial, que prometem acelerar ainda mais o processamento de dados e o treinamento de modelos de IA, impulsionando a próxima geração de aplicações.",
@@ -40,7 +34,7 @@ public class HomeController : Controller
             new(){
                 Id = 2,
                 Nome = "Sustentabilidade na Mineração de Criptomoedas: ",                
-                Categoria = tecnologia,
+                Categoria = categorias.Find(c => c.Id == 1),
                 DataPostagem = DateTime.Parse("10/06/2023"),
                 Descricao = "Meio Ambiente",
                 Texto = " Há um movimento crescente para tornar a mineração de criptomoedas mais sustentável. Novas tecnologias e iniciativas estão surgindo para reduzir o consumo de energia e a pegada de carbono, o que é um tema importante para o futuro da blockchain.",
@@ -50,7 +44,7 @@ public class HomeController : Controller
              new(){
                 Id = 3,
                 Nome = "Sustentabilidade na Mineração de Criptomoedas: ",                
-                Categoria = tecnologia,
+                Categoria = categorias.Find(c => c.Id == 1),
                 DataPostagem = DateTime.Parse("10/08/2025"),
                 Descricao = "Novidade",
                 Texto = "Há um movimento crescente para tornar a mineração de criptomoedas mais sustentável. Novas tecnologias e iniciativas estão surgindo para reduzir o consumo de energia e a pegada de carbono, o que é um tema importante para o futuro da blockchain.",
@@ -60,7 +54,7 @@ public class HomeController : Controller
             new(){
                 Id = 4,
                 Nome = "Cibersegurança em Foco com Ataques Ransomware",                
-                Categoria = tecnologia,
+                Categoria = categorias.Find(c => c.Id == 1),
                 DataPostagem = DateTime.Parse("01/08/2025"),
                 Descricao = "Segurança",
                 Texto = "Há um movimento crescente para tornar a mineração de criptomoedas mais sustentável. Novas tecnologias e iniciativas estão surgindo para reduzir o consumo de energia e a pegada de carbono, o que é um tema importante para o futuro da blockchain.",
@@ -70,7 +64,7 @@ public class HomeController : Controller
             new(){
                 Id = 5,
                 Nome = "Programação Low-Code e No-Code Ganhando Espaço",                
-                Categoria = tecnologia,
+                Categoria = categorias.Find(c => c.Id == 1),
                 DataPostagem = DateTime.Parse("10/09/2024"),
                 Descricao = "Low Code",
                 Texto = "Ferramentas de desenvolvimento low-code e no-code estão se popularizando, permitindo que pessoas com menos experiência em programação criem aplicativos e sistemas. ",
@@ -80,7 +74,7 @@ public class HomeController : Controller
             new(){
                 Id = 6,
                 Nome = "Audiobooks e Podcasts Literários em Ascensão:",                
-                Categoria = literatura,
+                Categoria = categorias.Find(c => c.Id == 2),
                 DataPostagem = DateTime.Parse("11/09/2024"),
                 Descricao = "Ouvir Livro",
                 Texto = "O consumo de literatura em formato de áudio continua a crescer exponencialmente. Plataformas de audiobooks e podcasts dedicados a resenhas, discussões e leitura de obras literárias estão popularizando o acesso a histórias e expandindo o alcance dos autores.",
@@ -90,7 +84,7 @@ public class HomeController : Controller
             new(){
                 Id = 7,
                 Nome = "Realidade Aumentada em Livros Infantis",                
-                Categoria = literatura,
+                Categoria = categorias.Find(c => c.Id == 2),
                 DataPostagem = DateTime.Parse("11/10/2024"),
                 Descricao = "IA para baixinhos",
                 Texto = " Editoras estão explorando a Realidade Aumentada (RA) para criar experiências de leitura mais imersivas, especialmente para crianças. Livros impressos que ganham vida com animações e elementos interativos através de um smartphone ou tablet estão se tornando uma tendência.",
@@ -100,7 +94,7 @@ public class HomeController : Controller
             new(){
                 Id = 8,
                 Nome = "Preservação Digital de Obras Raras: ",                
-                Categoria = literatura,
+                Categoria = categorias.Find(c => c.Id == 2),
                 DataPostagem = DateTime.Parse("07/09/2024"),
                 Descricao = "História",
                 Texto = "Projetos de digitalização em larga escala estão usando tecnologia avançada para preservar manuscritos antigos e obras literárias raras. Bibliotecas e instituições culturais estão tornando esses tesouros acessíveis globalmente, garantindo sua sobrevivência para futuras gerações de leitores e pesquisadores",
@@ -110,7 +104,7 @@ public class HomeController : Controller
             new(){
                 Id = 9,
                 Nome = "Plataformas de Leitura Social e Interativa:",                
-                Categoria = literatura,
+                Categoria = categorias.Find(c => c.Id == 2),
                 DataPostagem = DateTime.Parse("07/09/2024"),
                 Descricao = "Comunidade",
                 Texto = "Novas plataformas online estão surgindo para criar comunidades de leitores, permitindo discussões, marcações colaborativas e até mesmo a criação de histórias interativas onde o leitor escolhe o rumo da narrativa. Isso transforma a leitura em uma experiência mais engajadora e social.",
@@ -120,10 +114,18 @@ public class HomeController : Controller
         ];
 
         
+    }
+
+    public IActionResult Index()
+    {       
         return View(postagens);
     }
-    public IActionResult Postagem(){
-        return View();
+    public IActionResult Postagem(int id){
+
+        var postagem = postagens.Where(p => p.Id == id).SingleOrDefault();
+        if(postagem == null)
+        return NotFound();
+        return View(postagem);
     }
     public IActionResult Privacy()
     {
